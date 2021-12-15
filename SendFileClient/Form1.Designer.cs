decdfabb62af83ff693bbfecdee4e8ad19437d2a
me.ghafari <me.ghafari@ME-GHAFARI>
2021-12-15 18:12:44 +0330

    Add project files.

---
+++ SendFileClient/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SendFileClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.txt_FilePath = new System.Windows.Forms.TextBox();
+            this.btn_SendFile = new System.Windows.Forms.Button();
+            this.btn_Brows = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Path File :";
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(66, 30);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(141, 20);
+            this.txt_IP.TabIndex = 3;
+            this.txt_IP.Text = "172.20.3.31";
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(66, 79);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(141, 20);
+            this.txt_Port.TabIndex = 4;
+            this.txt_Port.Text = "5050";
+            // 
+            // txt_FilePath
+            // 
+            this.txt_FilePath.Location = new System.Drawing.Point(66, 137);
+            this.txt_FilePath.Name = "txt_FilePath";
+            this.txt_FilePath.Size = new System.Drawing.Size(474, 20);
+            this.txt_FilePath.TabIndex = 5;
+            this.txt_FilePath.TextChanged += new System.EventHandler(this.txt_FilePath_TextChanged);
+            // 
+            // btn_SendFile
+            // 
+            this.btn_SendFile.Location = new System.Drawing.Point(66, 173);
+            this.btn_SendFile.Name = "btn_SendFile";
+            this.btn_SendFile.Size = new System.Drawing.Size(112, 23);
+            this.btn_SendFile.TabIndex = 6;
+            this.btn_SendFile.Text = "Send File";
+            this.btn_SendFile.UseVisualStyleBackColor = true;
+            this.btn_SendFile.Click += new System.EventHandler(this.btn_SendFile_Click);
+            // 
+            // btn_Brows
+            // 
+            this.btn_Brows.Location = new System.Drawing.Point(546, 135);
+            this.btn_Brows.Name = "btn_Brows";
+            this.btn_Brows.Size = new System.Drawing.Size(42, 23);
+            this.btn_Brows.TabIndex = 7;
+            this.btn_Brows.Text = "...";
+            this.btn_Brows.UseVisualStyleBackColor = true;
+            this.btn_Brows.Click += new System.EventHandler(this.btn_Brows_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(618, 252);
+            this.Controls.Add(this.btn_Brows);
+            this.Controls.Add(this.btn_SendFile);
+            this.Controls.Add(this.txt_FilePath);
+            this.Controls.Add(this.txt_Port);
+            this.Controls.Add(this.txt_IP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.TextBox txt_FilePath;
+        private System.Windows.Forms.Button btn_SendFile;
+        private System.Windows.Forms.Button btn_Brows;
+    }
+}
+
---
+++ SendFileClient/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//--------------------------------------------------------------
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SendFileClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Brows_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                txt_FilePath.Text = op.FileName;
+            }
+        }
+
+        public void SendFile(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+            if (fileInfo.Length > 1024 * 50000)
+            {
+                MessageBox.Show("Your Size message is huge");
+                return;
+            }
+            byte[] fileNameByte = Encoding.ASCII.GetBytes(fileInfo.Name);
+            byte[] fileData = File.ReadAllBytes(fileName);
+            byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+            byte[] dataSend = new byte[4 + fileNameByte.Length + fileData.Length];
+            fileNameLen.CopyTo(dataSend, 0);
+            fileNameByte.CopyTo(dataSend, 4);
+            fileData.CopyTo(dataSend, 4 + fileNameByte.Length);
+
+            try
+            {
+                Socket sSendFile = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(txt_IP.Text), int.Parse(txt_Port.Text));
+                sSendFile.Connect(iPEndPoint);
+                sSendFile.Send(dataSend);
+                sSendFile.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("error => " + e.Message);
+            }
+        }
+
+        private void txt_FilePath_TextChanged(object sender, EventArgs e)
+        {
+            btn_SendFile.Enabled = txt_FilePath.Text.Trim() != "";
+        }
+
+        private void btn_SendFile_Click(object sender, EventArgs e)
+        {
+            SendFile(txt_FilePath.Text);
+        }
+    }
+}
